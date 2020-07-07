a1d5593f9337de744aa825e2463a3cf034383f36
cailliaud <cailliaud.a@gmail.com>
2020-07-07 16:40:05 +0200

    Init

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Person.css';
+import Radium from "radium";
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and i am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}/>
+        </div>
+    );
+}
+
+export default Radium(person);
